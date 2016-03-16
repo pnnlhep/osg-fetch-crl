530d545cf922c5be94ff6bd55fa879694a33e0cb
Kevin Fox <Kevin.Fox@pnnl.gov>
2016-03-16 15:12:18 -0700

    Push change so fetch crl failing is not fatal, killing the container.

--- start.sh
+++ start.sh
@@ -6,6 +6,12 @@ then
 	cp -a /etc/grid-security/certificates.orig/* /etc/grid-security/certificates/
 fi
 if [ "x$SKIP_INIT" == "x" ]; then
-	fetch-crl
+	#fetch-crl fails if any CA fails to refresh... It seems normal operation for one to fail. :/
+	if [ "x$FAIL_ON_FETCH_CRL_FAIL" != "x" ]; then
+		fetch-crl
+	else	
+		fetch-crl || true
+	fi
 fi
+touch /etc/grid-security/certificates/.fetch_crl_inited
 /usr/sbin/crond -n
